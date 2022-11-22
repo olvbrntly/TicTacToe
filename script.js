c1dac19ded1264791091ac6ff35f3f7d1d6ffc97
olive <olivia.brantley9@gmail.com>
2022-11-21 20:04:32 -0800

    basic player object

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
-//store a gameboard inside of an array inside of a gameboard object
+
+//modules (gameboard, displayControler)
+
+
+//gameboard object
+const gameBoard = (() => {
+    const game = [];
+
+
+});
+
+//factories (player);
+const Player = (name, symbol)=>{
+    name;
+    symbol;
+    getName = () => console.log(`Player's name is ${name}`);
+    getSymbol = () => console.log(`Player's symbol is ${symbol}`);
+    return {
+        getName,
+        getSymbol
+    }
+}
+
+const playerOne = Player('one', 'X');
+const playerTwo = Player('two','O');
+
+
+//store a game board inside of an array inside of a game board object
 //players are going to be stored in objects
 //probably going to want an object to control the flow of the game
-    //main goal is to have as little as globalcode as possible
-    //try to tiuck everything away inside of a module or factory
+    //main goal is to have as little as global code as possible
+    //try to tuck everything away inside of a module or factory
     //if you only need one thing use a module
-    //if you need multiples use facotries
+    //if you need multiples use factories
 
-//set up HTML and write JS function that will render the contents of the gameboard array to the webpage
+//set up HTML and write JS function that will render the contents of the game board array to the webpage
 //(for now just manually fill array with x's and o's)
 
-//build the functions that allow players to add marks to a specific spot on the oboard and then tie it to the DOM, letting players click on gameboard to place thei marker 
+//build the functions that allow players to add marks to a specific spot on the onboard and then tie it to the DOM, letting players click on game board to place their marker 
 //include logic that keeps players from playing in spots that are already taken
   
     //think carefully when each bit of logic should reside
-    //Each little piece of functionality should be able to fit in the game, player, or gamebaord objects
+    //Each little piece of functionality should be able to fit in the game, player, or game board objects
 
 //Build the logic that checks for when the game is over.
 //should check for 3 in a row and a tie
