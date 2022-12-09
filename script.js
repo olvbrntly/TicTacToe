48cd3419f8517daff732f7a8fec6c238c9d53447
olive <olivia.brantley9@gmail.com>
2022-12-08 20:01:00 -0800

    cleaned up code

--- script.js
+++ script.js
@@ -101,21 +101,21 @@ const GamePlay = (() => {
         if(GameBoard.board[this.id] === ' '){
             if(getPlayer() === player1){
                 this.textContent = "X";
-                GameBoard.board[this.id] =currentPlayer.getSymbol();
+                GameBoard.board[this.id] = currentPlayer.getSymbol();
                 console.log(this)
                
             }else{
                 this.textContent = "O";
-                GameBoard.board[this.id] =currentPlayer.getSymbol();
+                GameBoard.board[this.id] = currentPlayer.getSymbol();
             }
             nextPlayer();
        
-            DisplayController.gameOverMessage.textContent = `it is ${currentPlayer.getName()}'s turn!`
+            DisplayController.gameOverMessage.textContent = `It is ${currentPlayer.getName()}'s turn!`
+            rounds++
+            console.log(rounds);
         }
       
     }
-    rounds++
-
    function getPlayer() {
         return currentPlayer;
     }
@@ -127,14 +127,6 @@ const GamePlay = (() => {
             currentPlayer = player1;
         }
         return
-    }
-
-    
-  
-    
-
-    let getRounds = () => {
-        return rounds
     }
 
     const CheckWinner = () =>{
@@ -149,126 +141,25 @@ const GamePlay = (() => {
                                 [2,4,6]
                                         ]
 
-        
-    
-
-    
-        
     }
-
 
     function isGameOver(){
     return gameOver;
     }
 
+    //EVENT LISTENER FOR CLICKING EACH BOX ON TIC TAC TOE GRID
     DisplayController.gridDiv.forEach((box, index)=>{
         box.addEventListener('click', playRound);
     });
 
     return{
-
         currentPlayer,
-
-        
         playRound,
-        getRounds,
         isGameOver,
         nextPlayer,
-
-    }
+     }
     }
 )();
 
 
-    //    
-
-
-//basically, there are three outside modules. these cannot communicate with each other on their own
-//when they are passed into the gameplay module then that module can in fact communicate them
-//otherwise they do all theyre sepearte thing and allow for no gloabl code
-
-
-
-//notify mye if someone clicks a div and where they clicked
-//then hey can you play the current player there
-//does the current player have a win
-
-//player(instance od player factory)
-//gameboard(module purely state data)
-//displaycontolrrer(module, purely view)
-
-
-
-   //connects the array to the html div based on matching ID and index
-//    function attachIDs(){
-//         for(let i = 0; i < board.length; i++){
-//             let div = document.getElementById(`${i}`);
-//             board[i] = div;
-//         }
-//     };
-
-
-   // if(this.textContent == ''){
-        //     if(turns % 2 == 0){
-        //         this.textContent = "X";
-        //     }
-        //     else{
-        //         this.textContent = "O"
-        //     }
-        //     turns++
-        // }
-
-//game state: the data of the board, its ongoing state changes
-//game view: the Display Contoller(controls how the DOm looks)
-//game logic(the thing htat binds the state the view and the players)
-
-//probably going to want an object to control the flow of the game
-    //main goal is to have as little as global code as possible
-    //try to tuck everything away inside of a module or factory
-    //if you only need one thing use a module
-    //if you need multiples use factories
-
-//set up HTML and write JS function that will render the contents of the game board array to the webpage
-//(for now just manually fill array with x's and o's)
-
-//build the functions that allow players to add marks to a specific spot on the onboard and then tie it to the DOM, letting players click on game board to place their marker
-//include logic that keeps players from playing in spots that are already taken
-
-    //think carefully when each bit of logic should reside
-    //Each little piece of functionality should be able to fit in the game, player, or game board objects
-
-//Build the logic that checks for when the game is over.
-//should check for 3 in a row and a tie
-
-//Clean up interface to allow players to put in their name
-//include a button to start/restart the game
-//add a display element that congratulates the winning player
-
-//Optional --- create an AI so player can play against a computer
-
-
-//------------------------------------------------------------------------------
-
-//MISUNDERSTOOD - gameboard does not have to be dynamically created
-//going to code it into the HTML
-//BUT i did learn about using map and fill so not a total loss
-
- // const R = 3
-    // const C = 3;
-    // const val = " ";
-    // const gameArray = Array(R).fill().map(() => Array(C).fill(val));
-    // const gameBoardGrid = document.getElementById("gameBoard");
-
-    // //puts the array on the DOM
-    // function _displayArray(){
-    //     for(i = 0; i < gameArray.length; i++){
-    //         for(j = 0; j < gameArray.length; j++){
-    //             const div = document.createElement('div');
-    //             div.classList.add('box');
-    //             gameBoardGrid.appendChild(div);
-    //         }
-    //     }
-    // }
-
-    // _displayArray();
-    // console.log(gameArray);
+ 
